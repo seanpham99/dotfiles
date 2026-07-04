68e6abd48827c8b34328e4ba3a19d10a6566fe3f
RoadToDev101 <phamhoangson2611@gmail.com>
2026-07-04 22:46:29 +0700

    fix: move virtualenv to left prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -35,6 +35,7 @@
     # os_icon               # os identifier
     dir                     # current directory
     vcs                     # git status
+    virtualenv              # python virtual environment
     # =========================[ Line #2 ]=========================
     newline                 # \n
     # prompt_char           # prompt symbol
@@ -51,7 +52,7 @@
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    # virtualenv            # moved to left prompt
     anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
